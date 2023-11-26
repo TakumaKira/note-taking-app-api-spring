33ab2383740617eaf7f2b6e1a062e99819395443
Takuma Kira <takuma@kirakiraworx.com>
2023-11-26 16:49:55 +0900

    [dev] Create project

---
+++ src/main/java/com/takumakira/notetakingappapispring/NoteTakingAppApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.takumakira.notetakingappapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingAppApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakingAppApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takumakira/notetakingappapispring/NoteTakingAppApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takumakira.notetakingappapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingAppApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
